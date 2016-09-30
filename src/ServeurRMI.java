6fcc29e9a43e9549de749458f01974c55188e490
julienasp <julien.aspirot@usherbrooke.ca>
2016-09-29 23:21:26 -0400

    yolo

--- src/ServeurRMI.java
+++ src/ServeurRMI.java
@@ -1,13 +1,15 @@
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 import core.AdminToken;
 import core.Equation;
 
 
-public class ServeurRMI extends UnicastRemoteObject implements IRemoteEquation, IRemoteAdminHandler {
+public class ServeurRMI implements IRemoteEquation, IRemoteAdminHandler {
 	private AdminToken at = null;
 	/**
 	 * 
@@ -32,12 +34,21 @@ public class ServeurRMI extends UnicastRemoteObject implements IRemoteEquation, 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		try {
+			String stubName = "ServeurRMI";
 			AdminToken at = new AdminToken("rtCCTYgeUV&aP5w?");
 			ServeurRMI mServeur = new ServeurRMI(at);			
-			String url = "rmi://localhost/ServeurRMI";
+			/*String url = "rmi://localhost/" + stubName;
 			Naming.rebind(url, mServeur);
-			System.out.println("Enregistrement de l'objet avec l'url : " + url);			
-			System.out.println("Serveur lancé");
+			System.out.println("Enregistrement de l'objet avec l'url : " + url);*/
+			
+			
+			 	IRemoteEquation stub = (IRemoteEquation) UnicastRemoteObject.exportObject(mServeur, 0);
+				Registry registry = LocateRegistry.getRegistry("localhost");
+            	registry.rebind(stubName, stub);	 
+			
+			System.out.println("Enregistrement de l'objet avec le nom suivant : " + stubName);
+			System.out.println("Serveur lancé...");
+			
 		} catch (Exception e) {			
 			System.out.println(e.getMessage());			
 		}
